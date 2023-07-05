ef99473cbe523dd79b5c7019c2f57e49617e2463
shafiuyushawu <yishafiu@gmail.com>
2023-07-05 07:03:38 +0000

    implemented devise, create models for categories and group, included references

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  has_many :groups 
+  has_many :categories
+
+  def avatar_thumbnail 
+    if avatar.attached?
+      avatar.variant(resize: "150x150!").processed
+    else 
+      '/default_profile.jpg'
+    end
+  end
+end
